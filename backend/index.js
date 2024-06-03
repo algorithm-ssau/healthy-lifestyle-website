63b9e98f450143e06348b4186c9ef80fcb8ed904
Olga Aleksandrova <mer.deparis@yandex.ru>
2024-06-03 20:33:25 +0400

    validation auth

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,11 @@ import express from 'express';
 import jwt from 'jsonwebtoken';
 import mongoose from 'mongoose'
 
+import {validationResult} from 'express-validator'
+import {registerValidation}from './validations/auth.js'
+
+import UserModel from './models/Users.js'
+
 mongoose.
    connect('mongodb+srv://alexchelpek:01082003@cluster0.u9eeurr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',)
    .then(() => console.log('DB OK'))
@@ -10,25 +15,33 @@ mongoose.
 const app=express();
 app.use(express.json());
 
-app.get('/', (req, res)=> {
-res.send('Привет, мир!');
-}); 
 
-app.post('/auth/login',(req,res)=>{
-    console.log(req.body);
-    const token=jwt.sign(
-        {
-            email:req.body.email,
-            fullName:"Василий Иван",
 
-        },
-        'secret123',
-    )
+app.post('/auth/register',registerValidation,(req,res)=>{
+    const errors=validationResult(req);
+    if(!errors.isEmpty()){
+     return res.status(400).json(errors.array());
+    }
+ 
+    //const password=req.body.password;
+    //const salt= await bcrypt.genSalt(10);
+ 
+ 
+ 
+    const doc =new UserModel({
+     email:req.body.email,
+     fullName:req.body.fullName,
+     avatarUrl:req.body.avatarUrl,
+     passwordHash:req.body.avatarUrl,
+    });
+    
     res.json({
-        succes:true,
-        token,
+     succes:true,
     })
-})
+     
+  });
+    
+
 
 app.listen(4444,(err)=>
     {
