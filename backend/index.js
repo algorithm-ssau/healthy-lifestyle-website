5f7b965bf6d38761a2df19781632ad48f9a0ed31
Olga Aleksandrova <mer.deparis@yandex.ru>
2024-06-03 20:05:34 +0400

    post login

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,19 @@
 import express from 'express';
-
+import jwt from 'jsonwebtoken';
 
 const app=express();
-
+app.use(express.json());
 
 app.get('/', (req, res)=> {
 res.send('Привет, мир!');
 }); 
 
-
+app.post('/auth/login',(req,res)=>{
+    console.log(req.body);
+    res.json({
+        succes:true,
+    })
+})
 
 app.listen(4444,(err)=>
     {
