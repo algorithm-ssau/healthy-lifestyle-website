8c6a54fc5e154ffebd5eadae33a048398d81dc4a
Olga Aleksandrova <mer.deparis@yandex.ru>
2024-06-04 01:10:51 +0400

    рефакторинг файла index.js

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,14 @@
 import express from 'express';
-import bcrypt from 'bcrypt';
-import jwt from 'jsonwebtoken';
+
 import mongoose from 'mongoose'
 
-import {validationResult} from 'express-validator'
+
 import {registerValidation}from './validations/auth.js'
 
-import UserModel from './models/Users.js'
+
 import CheckAuth from './untils/CheckAuth.js'
+
+import * as UserController from './controllers/UserController.js';
 
 mongoose.
    connect('mongodb+srv://alexchelpek:01082003@cluster0.u9eeurr.mongodb.net/blog?retryWrites=true&w=majority&appName=Cluster0',)
@@ -18,129 +19,12 @@ const app=express();
 app.use(express.json());
 
 //автризация
-app.post('/auth/login',async(req,res)=> {
-    try{
-        const user=await UserModel.findOne({email:req.body.email});
-        if(!user){
-            return res.status(404).json({
-                message:'Пользователь не найден',
-            });
-        }
-        const isValidPass=await bcrypt.compare(req.body.password,user._doc.passwordHash);
-
-        if(!isValidPass){
-            return res.status(400).json({
-                message:'Неверный логин или пароль',
-            });
-
-        }
-        const token =jwt.sign(
-            {
-            _id:user._id,   
-            },
-        'secret123',
-           {             
-             expiresIn:'30d',
-    
-           },
-        );
-
-        const { passwordHash,...userData}=user._doc;
-
-    
-    res.json({
-        ...userData,
-        token,
-    });
-
-    }catch(err){
-
-        console.log(err);
-        res.status(500).json({message:'Не удалось авторизоваться',
-
-        });
-    }
-})
+app.post('/auth/login',UserController.login);
 //registration
-app.post('/auth/register',registerValidation,async(req,res)=>{
-    try{
-        const errors=validationResult(req);
-    if(!errors.isEmpty()){
-     return res.status(400).json(errors.array());
-    }
- 
-    const password=req.body.password;
-    const salt= await bcrypt.genSalt(10);
-    //в этой перменной будет храниться зашифрованный пароль(переменная passwordHash)
-    const hash=await bcrypt.hash(password,salt);
- 
- 
- 
-    const doc =new UserModel({
-     email:req.body.email,
-     fullName:req.body.fullName,
-     avatarUrl:req.body.avatarUrl,
-     passwordHash:hash,
-    });
-
-
-    //создаем пользователя в mongodb сохраняем его в документ
-    const user =await doc.save();
-
-
-    //id пользователя достаточно для проверок пользователя
-    //шифруем id
-    const token =jwt.sign({
-        _id:user._id,
-
-    },'secret123',
-       {
-         //срок хранения токена (перестанит быть валидным через 30 дней)
-         expiresIn:'30d',
-
-       }
-    );
-    const { passwordHash,...userData}=user._doc;
-
-    //важно что два res.json(user); нельзя возвращать express будет ругаться
-    res.json({
-        ...userData,
-        token,
-    });
-
-    }
-    catch(err){
-        console.log(err);
-        res.status(500).json({message:'Не удалось зарегестрироваться',
-
-        });
-    }
-     
-  });
+app.post('/auth/register',registerValidation,UserController.register);
    
   //роут на получение информации о себе(проверка можем ли получить информацию о себе)
-  app.get('/auth/me',CheckAuth,async(req,res)=>{
-    try{
-        const user= await UserModel.findById(req.userId);
-        if(!user){
-            return res.status(404).json({
-                message:'Пользователь не найден',
-
-            });
-        }
-    const { passwordHash,...userData}=user._doc;
-
-    
-    res.json(userData);
-
-    }
-    catch(err){
-        console.log(err);
-        res.status(500).json({message:'Нет доступа',});
-
-
-    }
-  })
+  app.get('/auth/me',CheckAuth,UserController.getMe);
 
 
 app.listen(4444,(err)=>
