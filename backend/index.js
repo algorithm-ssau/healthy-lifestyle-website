6c4db6fb30437dfa62e48e7102e56f1bf79bd909
DariaAntt <gtfdhtruyh@gmail.com>
2024-06-05 18:42:08 +0400

    Получение тегов из БД

--- backend/index.js
+++ backend/index.js
@@ -1,44 +1,53 @@
-import express from 'express';
+import express from "express";
 
-import mongoose from 'mongoose'
+//-------Даша------
+import cors from "cors";
 
-import {registerValidation, loginValidation, postCreateValidation}from './validations.js'
+import mongoose from "mongoose";
 
-import CheckAuth from './untils/CheckAuth.js'
+import {
+  registerValidation,
+  loginValidation,
+  postCreateValidation,
+} from "./validations.js";
 
-import * as UserController from './controllers/UserController.js';
-import * as PostController from './controllers/PostController.js';
+import CheckAuth from "./untils/CheckAuth.js";
 
-mongoose.
-   connect('mongodb+srv://alexchelpek:01082003@cluster0.u9eeurr.mongodb.net/blog?retryWrites=true&w=majority&appName=Cluster0',)
-   .then(() => console.log('DB OK'))
-   .catch((err) => console.log('DB error',err));
+import * as UserController from "./controllers/UserController.js";
+import * as PostController from "./controllers/PostController.js";
 
-const app=express();
+mongoose
+  .connect(
+    "mongodb+srv://alexchelpek:01082003@cluster0.u9eeurr.mongodb.net/blog?retryWrites=true&w=majority&appName=Cluster0"
+  )
+  .then(() => console.log("DB OK"))
+  .catch((err) => console.log("DB error", err));
+
+const app = express();
 app.use(express.json());
 
-//автризация
-app.post('/auth/login',loginValidation, UserController.login);
-//registration
-app.post('/auth/register',registerValidation,UserController.register);
-   
-//роут на получение информации о себе(проверка можем ли получить информацию о себе)
-app.get('/auth/me',CheckAuth, UserController.getMe);
+//----------Даша------
+app.use(cors());
 
+//автризация
+app.post("/auth/login", loginValidation, UserController.login);
+//registration
+app.post("/auth/register", registerValidation, UserController.register);
+
+//роут на получение информации о себе(проверка можем ли получить информацию о себе)
+app.get("/auth/me", CheckAuth, UserController.getMe);
 
 //роуты для статей
-app.get('/posts', PostController.getAll);
-app.get('/posts/:id',PostController.getOne);
-app.post('/posts',CheckAuth, postCreateValidation, PostController.create);
-app.delete('/posts/:id',CheckAuth, PostController.remove);
-app.patch('/posts/:id',PostController.update);
+app.get("/posts", PostController.getAll);
+app.get("/posts/tags", PostController.getLastTags);
+app.get("/posts/:id", PostController.getOne);
+app.post("/posts", CheckAuth, postCreateValidation, PostController.create);
+app.delete("/posts/:id", CheckAuth, PostController.remove);
+app.patch("/posts/:id", PostController.update);
 
-
-app.listen(4444,(err)=>
-    {
-        if(err){
-            return console.log(err);
-        }
-        console.log('Server OK');
+app.listen(4444, (err) => {
+  if (err) {
+    return console.log(err);
+  }
+  console.log("Server OK");
 });
-    
